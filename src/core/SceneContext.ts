59e188969e13880a7ea584026038a9da33f4c7b0
aunnava <anuragunnava@gmail.com>
2026-06-07 10:52:13 +0200

    Scene with orbital controls working

---
+++ src/core/SceneContext.ts
@@ -0,0 +1,48 @@
+import * as THREE from 'three';
+import { OrbitControls, ThreeMFLoader } from "three/examples/jsm/Addons.js";
+
+
+export class SceneContext{
+    readonly scene = new THREE.Scene();
+    readonly camera: THREE.PerspectiveCamera;
+    readonly renderer: THREE.WebGLRenderer;
+    readonly controls: OrbitControls;
+ 
+    constructor(canvas:HTMLCanvasElement) {
+
+        const {innerWidth:w, innerHeight:h}=window;
+    this.camera=new THREE.PerspectiveCamera(75,w/h,0.1,1000);
+    this.camera.position.set(3,2,5);
+
+
+    this.renderer=new THREE.WebGLRenderer({canvas,antialias:true});
+    this.renderer.setSize(w,h);
+    this.renderer.setPixelRatio(Math.min(window.devicePixelRatio,2));
+
+
+    this.controls=new OrbitControls(this.camera,this.renderer.domElement);
+    this.controls.enableDamping=true;
+
+    window.addEventListener("resize",this.handleResize);
+
+ }
+
+ render():void{
+    this.controls.update();
+    this.renderer.render(this.scene, this.camera);
+ }
+
+
+ private handleResize=():void=>{
+    const{innerWidth:w,innerHeight:h}=window;
+    this.camera.aspect=w/h;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(w,h);
+ };
+ dispose():void{
+    window.removeEventListener("resize",this.handleResize);
+    this.controls.dispose();
+    this.renderer.dispose();
+ }
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,16 @@
 import './style.css';
+import * as THREE from 'three';
+import { SceneContext } from './core/sceneContext';
 import { createScene } from './scene';
 
 
+const canvas=document.querySelector<HTMLCanvasElement>('#scene')!;
+const ctx=new SceneContext(canvas);
+const world=createScene(ctx.scene);
+
+const timer=new THREE.Timer();
+ctx.renderer.setAnimationLoop((time)=>{
+    timer.update(time);
+    world.update(timer.getDelta());
+    ctx.render();
+});
